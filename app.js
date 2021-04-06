27e497609eae95a2750fe15fb8e95696ec12389f
Kateryna911 <ptica.katia@gmail.com>
2021-04-06 15:31:45 +0300

    refactor: adjust semantic HTML and CSS-formatting according to rule 1.1 html-and-css

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement = function(taskString) {
     deleteButton.className = "btn delete";
     deleteButtonImg.src = './remove.svg';
     deleteButtonImg.className = "delete-img";
+    eleteButtonImg.setAttribute('alt', 'remove button');
     deleteButton.appendChild(deleteButtonImg);
 
 
